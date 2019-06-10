52c42a9c12685198267e794c0786722341d87204
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-06-10 18:28:13 +0530

    Responsive

--- src/components/NavBarResponsive.js
+++ src/components/NavBarResponsive.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import logo from '../images/logo.png'
+import { Link } from 'react-router-dom'
+import { logout } from '../actions/auth'
+import { connect } from 'react-redux';
 
-export default class Example extends React.Component {
+class NavBarResponsive extends React.Component {
   constructor(props) {
     super(props);
 
@@ -10,6 +13,10 @@ export default class Example extends React.Component {
     this.state = {
       collapsed: true
     };
+  }
+
+  logout = ()=>{
+    this.props.logout();
   }
 
   toggleNavbar() {
@@ -21,15 +28,37 @@ export default class Example extends React.Component {
     return (
       <div className="navbar-responsive-2" style={{zIndex:'1000'}}>
         <Navbar color="faded" style={{backgroundColor:'rgb(241,241,241)'}} light>
-          <NavbarBrand href="/" className="mr-auto"><img alt="logo"src={logo}></img></NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!this.state.collapsed} navbar>
+          <NavbarBrand href="/" className="mr-auto"><img  className="logo" alt="logo"src={logo}></img></NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2 ham-burger" />
+          <Collapse className="collapse" isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+              <Link to="/dashboard/my-sites">
+              <NavItem className="res-nav">
+                My Sites
               </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </Link>
+              <Link to="/dashboard/my-device">
+              <NavItem className="res-nav">
+                My Device
+              </NavItem>
+              </Link>
+              <Link to="/dashboard/my-requests">
+              <NavItem className="res-nav">
+                My Requests
+              </NavItem>
+              </Link>
+              <Link to="/dashboard/feedback">
+              <NavItem className="res-nav">
+                Feedback
+              </NavItem>
+              </Link>
+              <Link to="/dashboard/my-profile">
+              <NavItem className="res-nav">
+                My Profile
+              </NavItem>
+              </Link>
+              <NavItem onClick={this.logout} className="res-nav">
+                Logout
               </NavItem>
             </Nav>
           </Collapse>
@@ -38,3 +67,7 @@ export default class Example extends React.Component {
     );
   }
 }
+
+const mapStateToProps = state=>({})
+
+export default connect(mapStateToProps, { logout })(NavBarResponsive);
